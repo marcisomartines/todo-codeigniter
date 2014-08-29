8bc089593d7fd02c04fcf09326449fc55ec2007e
Marciso Gonzalez <marciso.gonzalez@gmail.com>
2014-08-29 11:36:54 -0400

    final config

--- application/config/config.php
+++ application/config/config.php
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = '0baf30835e83eb2b977dcd5d239c5218';
 
 /*
 |--------------------------------------------------------------------------
